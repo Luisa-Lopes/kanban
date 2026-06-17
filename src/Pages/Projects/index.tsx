bc0a91b38ad18d2f131d8c3e404b07f9eea93b36
Ana <aninhaslopess@gmail.com>
2026-06-17 16:18:26 -0300

    update projects list

--- src/Pages/Projects/index.tsx
+++ src/Pages/Projects/index.tsx
@@ -6,11 +6,42 @@ import { Button, Modal } from "../../Layout";
 import ModalProject from "./Components/ProjectModal";
 import Sidebar from "../../Layout/SideBar";
 import { Header } from "../../Layout/Header";
+import { IDashboardResume } from "./types";
 
 const Projects = () => {
-  const { openSidebar, setOpenSidebar } = useSidebar();
-
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const projects: IDashboardResume[] = [
+    {
+      id: "20",
+      title: "Sistema de Gestão de Projetos",
+      description:
+        "Plataforma web para gerenciamento de projetos, tarefas, equipes e sprints da empresa.",
+
+      createdAt: 1750086000000,
+      status: "Em andamento",
+      image: pastaImagem,
+    },
+    {
+      id: "30",
+      title: "Sistema de Gestão de Projetos",
+      description:
+        "Plataforma web para gerenciamento de projetos, tarefas, equipes e sprints da empresa.",
+      createdAt: 1750086000000,
+      status: "Em andamento",
+      image: pastaImagem,
+    },
+    {
+      id: "40",
+      title: "Sistema de Gestão de Projetos",
+      description:
+        "Plataforma web para gerenciamento de projetos, tarefas, equipes e sprints da empresa.",
+
+      createdAt: 1750086000000,
+      status: "Em andamento",
+      image: pastaImagem,
+    },
+  ];
 
   return (
     <div className="relative flex flex-col w-screen">
@@ -26,16 +57,12 @@ const Projects = () => {
         <section
           className="bg-white bg-opacity-50 h-full w-full m-auto rounded-md p-4 grid grid-cols-1 md:grid-cols-3  lg:grid-cols-4  gap-3 "
           style={{
-            gridTemplateRows: "repeat(3, 110px)",
+            gridTemplateRows: "repeat(3, 310px)",
           }}
         >
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} />
-          <Card image={pastaImagem} /> <Card image={pastaImagem} />
+          {projects?.map((project) => (
+            <Card project={project} />
+          ))}
         </section>
       </main>
 
---
+++ src/Pages/Projects/types.ts
@@ -0,0 +1,8 @@
+export interface IDashboardResume {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: number;
+  status: string;
+  image: string;
+}
